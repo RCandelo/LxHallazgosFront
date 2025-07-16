aab645c60e8431d39df5634f940b1a61f2a341c4
Ricardo Candelo <ricardocandelo07@gmail.com>
2025-07-16 15:09:32 -0500

    borar

--- LxHallazgos/src/services/api.config.js
+++
@@ -1,9 +0,0 @@
-const API_BASE_URL =  'http://localhost:3000/api';
-
-export const apiConfig = {
-  baseURL: API_BASE_URL,
-  timeout: 30000,
-  headers: {
-    'Content-Type': 'application/json',
-  }
-};
